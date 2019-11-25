9a6369cf6a8b904490d85e310875824164112add
mcbloch <alazeo@live.be>
2019-11-25 14:04:57 +0100

    move entrypoint to dockercompose file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,5 @@
 
 ping -c1 -q host.docker.internal 2>&1 | grep "bad address" >/dev/null && echo "$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}') host.docker.internal" >> /etc/hosts && echo "Hosts File Entry Added for Linux!!!!!" || :
 
-lein deps
+lein run migrate || exit
 lein run
