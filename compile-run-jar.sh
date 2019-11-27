221bd09b7425e7276996b8e8f7f340b33ece5c5c
mcbloch <alazeo@live.be>
2019-11-27 17:21:30 +0100

    forgot the jar step

--- compile-run-jar.sh
+++ compile-run-jar.sh
@@ -5,7 +5,7 @@
 
 ping -c1 -q host.docker.internal 2>&1 | grep "bad address" >/dev/null && echo "$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}') host.docker.internal" >> /etc/hosts && echo "Hosts File Entry Added for Linux!!!" || :
 
-
+lein uberjar
 
 java -jar target/uberjar/g2.jar run migrate || exit
 java -jar target/uberjar/g2.jar run
