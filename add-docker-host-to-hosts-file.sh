c83a5ce1ec649323c422b269bc6227af4ff45c3f
mcbloch <alazeo@live.be>
2019-11-27 18:00:19 +0100

    dunno

--- add-docker-host-to-hosts-file.sh
+++ add-docker-host-to-hosts-file.sh
@@ -3,4 +3,5 @@
 # Fix for the host.docker.internal bug
 # https://github.com/docker/for-linux/issues/264
 
-echo "$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}') host.docker.internal" >> /etc/hosts && echo "Hosts File Entry Added for Linux!!!" || :
+echo "$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}') host.docker.internal" >> /etc/hosts && \
+    echo "Hosts file updated"
