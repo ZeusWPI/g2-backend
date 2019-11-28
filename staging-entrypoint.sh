7dc29efa8a41801cceee5117fbe23a58db4a6456
mcbloch <alazeo@live.be>
2019-11-28 21:49:30 +0100

    now for real

--- staging-entrypoint.sh
+++ staging-entrypoint.sh
@@ -3,6 +3,6 @@
 ./add-docker-host-to-hosts-file.sh
 
 # migrate and run the application
-# java -Dconf=dev-config.edn -jar target/uberjar/g2.jar migrate && \
-# java -Dconf=dev-config.edn -jar target/uberjar/g2.jar
+java -Dconf=dev-config.edn -jar target/uberjar/g2.jar migrate && \
+java -Dconf=dev-config.edn -jar target/uberjar/g2.jar
 
