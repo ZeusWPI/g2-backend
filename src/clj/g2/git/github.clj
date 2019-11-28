7c60d39433c2a18dc916c1feae27c70b5819ac5c
Francis <francisklinck@gmail.com>
2019-11-28 13:46:18 +0100

    update repo key

--- src/clj/g2/git/github.clj
+++ src/clj/g2/git/github.clj
@@ -25,13 +25,13 @@
    (let [response-body (:body (http/get (str base-url "/orgs" "/" (env :github-organization) "/repos?per_page=100") {:as :json}))
          filtered_body (map #(select-keys %1 [:id :name :description :html_url]) response-body)
          converted_body (map #(set/rename-keys %1 {:html_url :url
-                                                   :id :git_id}) filtered_body)
+                                                   :id :repo_id}) filtered_body)
          remote_repo_map (reduce (fn [acc repo] (assoc acc (:git_id repo) repo)) {} converted_body)
-         remote_ids (set (map :git_id converted_body))
+         remote_ids (set (map :repo_id converted_body))
          ; Local repos, id's and map
          local-repos (db/get-repos) ;TODO filter on github
          local_repo_map (reduce (fn [acc repo] (assoc acc (:git_id repo) repo)) {} local-repos)
-         local_ids (set (map :git_id local-repos))
+         local_ids (set (map :repo_id local-repos))
          ; Calculated id sets
          new_ids (clojure.set/difference remote_ids local_ids)
          common_ids (clojure.set/intersection remote_ids local_ids)
@@ -43,6 +43,7 @@
      (log/debug (format "Creating %d new repos" (count new_ids)))
      (doseq [git_id new_ids]
        (let [remote_repo (get remote_repo_map git_id)]
+         (log/debug remote_repo)
          (db/create-repo! remote_repo)))
      ;Update local repositories with remote data
      (log/debug (format "Updating %d of %d repos" (count update_ids) (count common_ids)))
