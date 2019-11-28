85fb60f929ddac841e6992773a3f39fab4f223ba
mcbloch <alazeo@live.be>
2019-11-28 21:42:12 +0100

    test: remove unique constraints

--- resources/migrations/20190322150828-add-table-git-links.up.sql
+++ resources/migrations/20190322150828-add-table-git-links.up.sql
@@ -2,6 +2,5 @@ CREATE TABLE repository_providers
 (
     ID           integer      not null auto_increment primary key,
     name         VARCHAR(191),
-    access_token VARCHAR(255) not null,
-    CONSTRAINT UC_Provider UNIQUE (name)
+    access_token VARCHAR(255) not null
 );
