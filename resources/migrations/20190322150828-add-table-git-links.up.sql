d490a93f6ca242beee70d8c2ec1839b5ed5a6c4a
mcbloch <alazeo@live.be>
2019-11-28 21:25:45 +0100

    forgot this constraint

--- resources/migrations/20190322150828-add-table-git-links.up.sql
+++ resources/migrations/20190322150828-add-table-git-links.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE repository_providers
 (
     ID           integer      not null auto_increment primary key,
-    name         VARCHAR(255),
+    name         VARCHAR(191),
     access_token VARCHAR(255) not null,
     CONSTRAINT UC_Provider UNIQUE (name)
 );
