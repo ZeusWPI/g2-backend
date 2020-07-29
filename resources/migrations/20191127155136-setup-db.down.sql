4c79d7f952091b4d8201b7612d19cf5a966c41fa
mcbloch <alazeo@live.be>
2020-07-29 23:45:15 +0200

    Separate namedtags from the tags controller more generics
    More credits to fkd13 boi

--- resources/migrations/20191127155136-setup-db.down.sql
+++ resources/migrations/20191127155136-setup-db.down.sql
@@ -12,7 +12,7 @@ drop table if exists named_tags cascade;
 --;;
 drop table if exists projects cascade;
 --;;
-drop table if exists repo_default_tag_mapping cascade;
+DROP TABLE IF EXISTS named_tags;
 --;;
 drop table if exists repository_labels cascade;
 --;;
